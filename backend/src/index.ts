e04769e34c987c15f04db79de2a7ae8db2709665
jitheshm <jitheshmjithooz@gmail.com>
2024-08-24 19:15:36 +0530

    validation added for image upload

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import ocrRouter from './routes/ocr';
 
 const app = express();
@@ -7,6 +7,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use('/api', ocrRouter);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(400).json({ error: err.message });
+
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
--- backend/src/routes/ocr.ts
+++ backend/src/routes/ocr.ts
@@ -1,11 +1,25 @@
-import { Router } from 'express';
-import multer from 'multer';
+import { Request, Router } from 'express';
+import multer, { FileFilterCallback } from 'multer';
 import ocrController from '../controllers/ocrController';
+const path = require('path');
 
 
 const router = Router();
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+
+const fileFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
+    const allowedTypes = /jpeg|jpg|png/;
+    const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = allowedTypes.test(file.mimetype);
+
+    if (extname && mimetype) {
+        cb(null, true);
+    } else {
+        cb(new Error('Invalid file type. Only JPEG, PNG files are allowed.'));
+    }
+};
+
+const upload = multer({ storage: storage, fileFilter: fileFilter });
 
 router.post('/ocr', upload.array('images', 2), ocrController);
 
