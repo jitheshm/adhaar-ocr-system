627e83f97ed238f9890193b6089ce7d3eed95375
jitheshm <jitheshmjithooz@gmail.com>
2024-08-25 14:37:46 +0530

    output data display component added , loading component added

---
+++ frontend/src/components/ParsedOutputComponent.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { ExtractedInfo } from '../types/ExtractedInfo'
+
+function ParsedOutputComponent({ parseData }: { parseData: ExtractedInfo | null }) {
+    return (
+        <>
+            <div className='flex items-center w-full h-screen'>
+                <div className='p-5  border m-auto w-6/12'>
+
+                    <form className="max-w-md mx-auto">
+                        <p className=' font-semibold'>Parsed Data</p>
+                        <div className="grid md:grid-cols-2 mt-4 md:gap-6">
+                            <div className="relative z-0 w-full mb-5 group">
+                                <input type="text" name="floating_uid" id="floating_uid" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required value={parseData?.UID ?? ''} />
+                                <label htmlFor="floating_uid" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Aadhaar Number</label>
+                            </div>
+                            <div className="relative z-0 w-full mb-5 group">
+                                <input type="text" name="floating_name" id="floating_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required value={parseData?.Name ?? ''} />
+                                <label htmlFor="floating_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Name on Aadhaar</label>
+                            </div>
+                        </div>
+
+                        <div className="grid md:grid-cols-2 mt-4 md:gap-6">
+                            <div className="relative z-0 w-full mb-5 group">
+                                <input type="text" name="floating_dob" id="floating_dob" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required value={parseData?.DOB ?? ''} />
+                                <label htmlFor="floating_dob" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Date of birth</label>
+                            </div>
+                            <div className="relative z-0 w-full mb-5 group">
+                                <input type="text" name="floating_gender" id="floating_gender" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required value={parseData?.Gender ?? ''} />
+                                <label htmlFor="floating_gender" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Gender</label>
+                            </div>
+                        </div>
+                        <div className="relative z-0 w-full mb-5 group">
+                            <input type="text" name="floating_address" id="floating_address" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required value={parseData?.address ?? ''} />
+                            <label htmlFor="floating_address" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Address</label>
+                        </div>
+                        <div className="grid md:grid-cols-2 mt-4 md:gap-6">
+                            <div className="relative z-0 w-full mb-5 group">
+                                <input type="text" name="floating_pincode" id="floating_dob" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required value={parseData?.pincode ?? ''} />
+                                <label htmlFor="floating_pincode" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Pincode</label>
+                            </div>
+
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default ParsedOutputComponent
---
+++ frontend/src/types/ExtractedInfo.ts
@@ -0,0 +1,10 @@
+export type ExtractedInfo = {
+    UID: string | null;
+    Name: string | null;
+    DOB: string | null;
+    Gender: string | null;
+    address: string | null;
+    pincode: string | null;
+    age_band: string;
+    isUidSame: string
+};
